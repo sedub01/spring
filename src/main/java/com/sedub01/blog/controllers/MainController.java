b9301429eebc212f288a1b139017b90bb15c8e43
sedub01 <dubenkov.semen@mail.ru>
2021-07-24 23:01:36 +0300

    добавлена главная страница и зависимости

---
+++ src/main/java/com/sedub01/blog/controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.sedub01.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**Отвечает за обработку всех переходов на сайте */
+@Controller
+public class MainController {
+    //указывается, какой именно URL адрес мы сейчас обрабатываем
+	@GetMapping("/") //"/" - главная страница
+    //при переходе на главную страницу вызывается данный url
+	public String home(Model model) {
+		model.addAttribute("title", "Главная страница");
+		return "home";
+	}
+
+}
