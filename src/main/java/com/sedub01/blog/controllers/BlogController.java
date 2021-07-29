235e8aaafe8973b6e50bb0ab1e8d4fe8a0005f75
sedub01 <dubenkov.semen@mail.ru>
2021-07-29 21:47:42 +0300

    Добавил файл с собственными пояснениями

--- src/main/java/com/sedub01/blog/controllers/BlogController.java
+++ src/main/java/com/sedub01/blog/controllers/BlogController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BlockController {
+public class BlogController {
     @GetMapping("/blog")
     public String blogMain(Model model){
         return "blog-main";
