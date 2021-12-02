30e06fa51d40c973f591d39f25b872a63c747934
sedub01 <dubenkov.semen@mail.ru>
2021-12-02 16:24:17 +0300

    Начал урок

---
+++ lessons/Lesson11.Annotations3/src/main/java/springcourse/MusicPlayer.java
@@ -0,0 +1,16 @@
+package springcourse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MusicPlayer {
+    @Autowired
+    @Qualifier("classicalMusic")
+    private Music music;
+
+    public String playMusic(){
+        return "Playing -> " + music.getSong();
+    }
+}
