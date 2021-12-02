4f17e9967f11403f48faac06191ced39a83168b5
sedub01 <dubenkov.semen@mail.ru>
2021-12-02 16:59:42 +0300

    Added new lesson

--- lessons/Lesson11.Annotations3/src/main/java/springcourse/MusicPlayer.java
+++ lessons/Lesson11.Annotations3/src/main/java/springcourse/MusicPlayer.java
@@ -4,13 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+enum Genres{
+    ROCK,
+    CLASSICAL
+}
+
 @Component
 public class MusicPlayer {
-    @Autowired
-    @Qualifier("classicalMusic")
-    private Music music;
+    private Music music1;
+    private Music music2;
 
-    public String playMusic(){
-        return "Playing -> " + music.getSong();
+    @Autowired
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
+    }
+
+    public String playMusic(Genres genre){
+        if (genre == Genres.ROCK)
+        return "Playing -> " + music1.getSong();
+        return "Playing -> " + music2.getSong();
     }
 }
---
+++ lessons/Lesson11.Annotations3/src/main/java/springcourse/TestSpring.java
@@ -0,0 +1,15 @@
+package springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
+        context.close();
+    }
+}
