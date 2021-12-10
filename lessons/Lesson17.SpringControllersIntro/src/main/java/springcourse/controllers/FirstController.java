631239a318cdb5e4c6743f18625a4c00fd3226e8
sedub01 <dubenkov.semen@mail.ru>
2021-12-10 21:40:41 +0300

    added lesson 17

---
+++ lessons/Lesson17.SpringControllersIntro/src/main/java/springcourse/controllers/FirstController.java
@@ -0,0 +1,20 @@
+package springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first") //все адреса будут иметь префикс first
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(){
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage(){
+        return "first/goodbye";
+    }
+}
